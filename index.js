f7f177d29efd8422d2d7c82a073b60e31a3c4078
FomalhautLeo <fomalhautleo@163.com>
2024-11-13 14:51:44 +0800

    :tada: challenge done!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +11,41 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+let shortUrls = []
+
+app.use('/api/shorturl', bodyParser.urlencoded({ extended: true })); // 解析 urlencoded 格式的请求体
+app.use('/api/shorturl', bodyParser.json()); // 解析 JSON 格式的请求体
+app.post('/api/shorturl', (req, res) => {
+  console.log("Post body: ", req.body);
+  const postUrl = req.body.url;
+  let resJson = {}
+  if (/^https?:.*$/.test(postUrl)) {
+    resJson = {
+      original_url: postUrl,
+      short_url: shortUrls.length,
+    }
+    shortUrls.push(postUrl);
+  } else {
+    resJson = {
+      error: "invalid url",
+    }
+  }
+  res.json(resJson);
+});
+
+app.get('/api/shorturl/:short?', (req, res) => {
+  res.redirect(shortUrls[req.params.short]);
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
